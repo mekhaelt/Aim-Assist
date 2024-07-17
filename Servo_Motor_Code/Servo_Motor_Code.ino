510c65ee024498d381e221942da1afe749be6e75
Raiyan Aaijaz <raiyanjaz@gmail.com>
2024-07-17 05:28:05 -0400

    Update Servo_Motor_Code.ino

--- Servo_Motor_Code/Servo_Motor_Code.ino
+++ Servo_Motor_Code/Servo_Motor_Code.ino
@@ -1,36 +1,49 @@
 #include <Servo.h>
 
-int buttonPin = 4;
-int button2Pin = 7;
+Servo myServo;
 
-int servoPin = 3;
-Servo Servo1;
-int servoPosition = 0;
+const int signalPin = A0; //analog pin connected to the circuit output
+const int motorControlPin = 9; //digital ~ pin connected to the motor control wire
+const int N = 10; //numbers of readings for dynamic threshold
+int readings[N]; //array of size 10 to hold the readings
+int readIndex = 0; // index to track the current reading position
+int readingsTotal = 0; //sum of readings for average
+int averageReading = 0; //dynamic threshold
 
 void setup() {
-  // put your setup code here, to run once:
-  Servo1.attach(servoPin);
-  Servo1.write(servoPosition);
 
   Serial.begin(9600);
+  pinMode(motorControlPin, OUTPUT); //setting motor control pin as output
+  digitalWrite(motorControlPin, LOW); //setting motor to be off initially
+
+  for(int i = 0; i <= 10; i++){
+    readings[i] = 0; //initializing all readings to zero
+  }
+
+  myServo.attach(motorControlPin); //attaching the motor to the motor control pin
+  myServo.write(10); //starting position of the motor
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int buttonState = digitalRead(buttonPin);
-  int button2State = digitalRead(button2Pin);
 
-  Servo1.write(servoPosition);
+//  readingsTotal = readingsTotal - readings[readIndex]; //removed the oldest reading from the total
+  int signalValue = analogRead(signalPin); //read the signal
+  Serial.println(signalValue);
 
-  if (buttonState == HIGH && servoPosition < 180) {
-    Servo1.write(servoPosition);
-    servoPosition++;
-    delay(5);
-  } else if (button2State == HIGH && servoPosition >= 0) {
-    Servo1.write(servoPosition);
-    servoPosition--;
-    delay(5);
+
+//  if(signalValue > averageReading){
+  if (signalValue > 750){
+
+    myServo.attach(9);
+    digitalWrite(motorControlPin, HIGH); 
+    myServo.write(110); //adjusts the motor 90 degrees
+    delay(2500);
+    myServo.write(10);
+    delay(2500);
+    digitalWrite(motorControlPin, LOW);
+
   }
+  myServo.detach();
 
 }
